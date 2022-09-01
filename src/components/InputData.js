83836f46637ccbf9f29a77d160b58548be351662
rachidbend <rachidbendaghor@gmail.com>
2022-09-01 19:57:46 +0100

    styled both results and inputs a lot but they still need work

--- src/components/InputData.js
+++ src/components/InputData.js
@@ -13,7 +13,7 @@ export const InputData = props => {
   // what this component should render
   return (
     <div className="input-data">
-      <div>
+      <div className="input-data__section">
         <h3>Bill</h3>
         <input
           type="number"
@@ -24,54 +24,70 @@ export const InputData = props => {
           placeholder={'0'}
         />
       </div>
-      <div>
-        <h3>tip percentage</h3>
-        <form>
-          <label htmlFor="tip5">5%</label>
-          <input
-            className="tip-input"
-            type="checkbox"
-            name="tip5"
-            id="tip5"
-            onChange={handleCheckBox}
-            value={5}
-          />
-          <label htmlFor="tip10">10%</label>
-          <input
-            className="tip-input"
-            type="checkbox"
-            name="tip10"
-            id="tip10"
-            onChange={handleCheckBox}
-            value={10}
-          />
-          <label htmlFor="tip15">15%</label>
-          <input
-            className="tip-input"
-            type="checkbox"
-            name="tip15"
-            id="tip15"
-            onChange={handleCheckBox}
-            value={15}
-          />
-          <label htmlFor="tip25">25%</label>
-          <input
-            className="tip-input"
-            type="checkbox"
-            name="tip25"
-            id="tip25"
-            onChange={handleCheckBox}
-            value={25}
-          />
-          <label htmlFor="tip50">50%</label>
-          <input
-            className="tip-input"
-            type="checkbox"
-            name="tip50"
-            id="tip50"
-            onChange={handleCheckBox}
-            value={50}
-          />
+      <div className="input-data__section">
+        <h3>Select Tip %</h3>
+        <form className="tip-form">
+          <div>
+            <label htmlFor="tip5">5%</label>
+            <input
+              className="tip-input"
+              type="checkbox"
+              name="tip5"
+              id="tip5"
+              onChange={handleCheckBox}
+              value={5}
+            />
+            <span className="checkmark"></span>
+          </div>
+          <div>
+            <label htmlFor="tip10">10%</label>
+            <input
+              className="tip-input"
+              type="checkbox"
+              name="tip10"
+              id="tip10"
+              onChange={handleCheckBox}
+              value={10}
+            />
+            <span className="checkmark"></span>
+          </div>
+          <div>
+            <label htmlFor="tip15">15%</label>
+            <input
+              className="tip-input"
+              type="checkbox"
+              name="tip15"
+              id="tip15"
+              onChange={handleCheckBox}
+              value={15}
+            />
+            <span className="checkmark"></span>
+          </div>
+          <div>
+            <label htmlFor="tip25">25%</label>
+            <input
+              className="tip-input"
+              type="checkbox"
+              name="tip25"
+              id="tip25"
+              onChange={handleCheckBox}
+              value={25}
+            />
+            <span className="checkmark"></span>
+          </div>
+          <div>
+            <label htmlFor="tip50">50%</label>
+            <input
+              className="tip-input"
+              type="checkbox"
+              name="tip50"
+              id="tip50"
+              onChange={handleCheckBox}
+              value={50}
+            />
+            <span className="checkmark"></span>
+          </div>
+
           <input
             type="number"
             name="tipCustom"
@@ -81,8 +97,8 @@ export const InputData = props => {
           />
         </form>
       </div>
-      <div>
-        <h3>number of people</h3>
+      <div className="input-data__section">
+        <h3>Number of People</h3>
         <input
           type="number"
           name="numberOfPeople"
