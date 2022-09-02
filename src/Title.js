ffbae5903e7886c2b2f661cea054df4f3cd5eae7
rachidbend <rachidbendaghor@gmail.com>
2022-09-02 15:18:15 +0100

    almost done with the styling

--- src/Title.js
+++ src/Title.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import logo from './images/logo.svg';
 
-export const Title = function () {
+export const Title = function() {
   return (
-    <div>
-      <h1 className="heading heading--primary">
-        spli
-        <br />
-        tter
-      </h1>
+    <div className="logo__container">
+      <img src={logo} alt="logo" className="logo" />
     </div>
   );
 };
+
+/* <h1 className="heading heading--primary">
+spli
+<br />
+tter
+</h1> */
--- src/components/InputData.js
+++ src/components/InputData.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export const InputData = props => {
@@ -10,11 +10,24 @@ export const InputData = props => {
   const numberOfPeople = props.numberOfPeople;
   const handleNumOfPeople = props.handleNumOfPeople;
 
+  useEffect(() => {
+    console.log(numberOfPeople);
+    if (numberOfPeople === 0 || numberOfPeople === '') {
+      document
+        .querySelector('.input-data__section--people')
+        .classList.add('input-data__section--people--empty');
+    } else {
+      document
+        .querySelector('.input-data__section--people')
+        .classList.remove('input-data__section--people--empty');
+    }
+  });
+
   // what this component should render
   return (
     <div className="input-data">
       <div className="input-data__section">
-        <h3>Bill</h3>
+        <h3 className="heading--bill">Bill</h3>
         <input
           type="number"
           name="bill"
@@ -25,7 +38,7 @@ export const InputData = props => {
         />
       </div>
       <div className="input-data__section">
-        <h3>Select Tip %</h3>
+        <h3 className="heading--tip">Select Tip %</h3>
         <form className="tip-form">
           <div>
             <label htmlFor="tip5">5%</label>
@@ -97,8 +110,11 @@ export const InputData = props => {
           />
         </form>
       </div>
-      <div className="input-data__section">
-        <h3>Number of People</h3>
+      <div className="input-data__section input-data__section--people input-data__section--people--empty  ">
+        <div className="people--container">
+          <h3 className="heading--people">Number of People</h3>
+          <p className="zero-warning">Can't be zero</p>
+        </div>
         <input
           type="number"
           name="numberOfPeople"
